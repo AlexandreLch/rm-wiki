98c9f6d05702ca8d69d415aafb2b3d2beb648050
Alexandre Lellouche <alexandre.lellouche@hetic.net>
2022-10-05 16:34:54 +0200

    Initial Commit

---
+++ RMWiki/RMWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMWikiApp.swift
+//  RMWiki
+//
+//  Created by Alexandre Lellouche on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RMWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
